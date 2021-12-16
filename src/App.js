c8df5912f065b09ec0fae3701d63ecd8fa436a87
Tomwf <chowkirthmas@gmail.com>
2021-12-16 16:24:44 +0100

    Remove boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const title = 'Blog Post'
+  const body = 'This is my blog post'
+  const comments = [
+    {id: 1, text: 'Comment 1'},
+    {id: 2, text: 'Comment 2'},
+    {id: 3, text: 'Comment 3'}
+  ]
+  const showComments = true
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+
+      {showComments && (
+        <div className="comments">
+          <h3>Comments ({comments.length})</h3>
+          <ul>
+            {comments.map(comment => (
+              <li key={comment.id}>{comment.text}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
